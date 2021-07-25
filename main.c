edca4dd4d618c159728381d8cb46a8394698f1b0
sabinprj <sabin.parajuli13@gmail.com>
2021-07-25 22:47:08 +0545

    Remove trailing white spaces

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 /*
  * navipage - multi-file pager for watching YouTube videos
  * Copyright (C) 2021 Sebastian LaVine <mail@smlavine.com>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
@@ -111,7 +111,7 @@ typedef struct {
 	int used;
 
 	/* Pointer to the array. */
-	char **v; 
+	char **v;
 } FileList;
 
 typedef struct {
@@ -342,7 +342,7 @@ compare_path_basenames(const void *p1, const void *p2)
 	 * basename().
 	 */
 	char *base[2], *copy[2];
-	
+
 	/* p1 and p2 are pointers to pointers to char, but strcmp() arguments
 	 * are "pointers to char", hence the following cast plus dereference.
 	 * (Paraphrasing `man 3 qsort`)
